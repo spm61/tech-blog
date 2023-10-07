8844738b4aec2cc20032365e284b3d41c12f1ca4
spmoroney <spmoroney@att.net>
2023-10-07 09:48:22 -0400

    define model structure.

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+//The User Model
+class User extends Model {
+    checkPassword(loginPW) { //check the password for each instance.
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+//initialize the model.
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4] //it's gotta be at least 4 characters long.
+            }
+        }  
+    },
+
+    {
+        hooks: { //hash the password before creating or updating the user.
+            async beforeCreate(newUserData) {
+                newUserData.password = await bycrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        }, 
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
